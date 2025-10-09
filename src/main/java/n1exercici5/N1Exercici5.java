379d35524aaee6ba2598f911a1a9e5f8a05d9584
Esther Arias <eariasvalor@gmail.com>
2025-10-09 13:05:00 +0200

    fix: add message to method deserializeObject()

--- src/main/java/n1exercici5/N1Exercici5.java
+++ src/main/java/n1exercici5/N1Exercici5.java
@@ -112,9 +112,12 @@ public class N1Exercici5 {
 
     public static void deserializeObject(String filePath) {
         try {
+            System.out.println("\n--- Deserializing objects from file: " + filePath + " ---");
             ObjectInputStream readFile = new ObjectInputStream(new FileInputStream(filePath));
             Worker[] retrievedWorkers = (Worker[]) readFile.readObject();
             readFile.close();
+
+            System.out.println("Objects successfully deserialized. Workers loaded:");
             for (Worker w : retrievedWorkers) {
                 System.out.println(w);
             }
