9bd46d7b490fd6a74a3f84a22be2dca26303f2bc
Esther Arias <eariasvalor@gmail.com>
2025-10-13 13:24:54 +0200

    fix: extracted methods into classes

--- src/main/java/n1exercici2/N1Exercici2.java
+++ src/main/java/n1exercici2/N1Exercici2.java
@@ -1,5 +1,7 @@
 package n1exercici2;
 
+import common_classes.ListFilesRecursive;
+
 import java.io.File;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -12,33 +14,11 @@ public class N1Exercici2 {
         File directory = new File(dirPath);
 
         if (directory.exists() && directory.isDirectory()) {
-            listFilesRecursive(directory, 0);
+            ListFilesRecursive.listFilesRecursive(directory, 0);
         } else {
             System.out.println("The directory doesn't exist or is not valid.");
         }
 
     }
 
-    public static void listFilesRecursive(File directory, int level) {
-        String indent = "  ".repeat(level);
-        File[] files = directory.listFiles();
-
-        if (files != null) {
-            for (File file : files) {
-                String formattedDate = formatLastModified(file.lastModified());
-                if (file.isDirectory()) {
-                    System.out.println(indent + "D: " + file.getName() + ". Last modified: " + formattedDate);
-                    listFilesRecursive(file, level + 1);
-                } else {
-                    System.out.println(indent + "F: " + file.getName() + ". Last modified: " + formattedDate);
-                }
-            }
-        }
-    }
-
-    private static String formatLastModified(long lastModifiedMillis) {
-        LocalDateTime lastModified = LocalDateTime.ofInstant(Instant.ofEpochMilli(lastModifiedMillis), ZoneId.systemDefault());
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        return lastModified.format(formatter);
-    }
 }
