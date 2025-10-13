1a41f866268a2e320c5519c674b828660fbcdd66
Esther Arias <eariasvalor@gmail.com>
2025-10-13 13:43:20 +0200

    feat: add validations

--- src/main/java/n1exercici3/N1Exercici3.java
+++ src/main/java/n1exercici3/N1Exercici3.java
@@ -11,6 +11,12 @@ import java.io.IOException;
 
 public class N1Exercici3 {
     public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.out.println("Error: please specify the directory path as an argument.");
+            System.out.println("Please use: java n1exercici1.N1Exercici1 <directory_path>");
+            return;
+        }
+
         String dirPath = args[0];
         File directory = new File(dirPath);
 
